93d989cc0a8764ef45745f46188f3800887faf2d
金子官 <18917950960@163.com>
2023-11-17 23:52:46 +0800

    init

---
+++ BR_Tree_Node.h
@@ -0,0 +1,15 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-11-17 23:10:11
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-11-17 23:10:32
+ * @FilePath: /black-red-tree/BR_Tree_Node.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#ifndef BR_TREE_NODE
+#define BR_TREE_NODE
+
+
+
+
+#endif
---
+++ README.md
@@ -0,0 +1,10 @@
+<!--
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-11-17 23:11:48
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-11-17 23:12:33
+ * @FilePath: /black-red-tree/README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+# Black-Red-Tree
+尝试用c++实现红黑树，算是练手用的小项目:P
